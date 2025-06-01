ded444430cef083e1778fb3acbe40cf4d30fbb26
Akshay <akshay.allen26200@gmail.com>
2025-06-01 12:47:33 +0530

    test

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.NODE_ENV === 'production' 
-    ? "https://portfolio-nvtm.vercel.app/api/v1"
+    ? "https://portfolio-nvtm.vercel.app/api/v1"  
     : "http://localhost:5001/api/v1";
 
 const axiosInstance = axios.create({
